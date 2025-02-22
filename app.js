67fbb89ea1cb5b821c044537c8f0e0a448f28ab9
HUNAIN882 <hunaintoufeeq@gmail.com>
2025-02-21 16:32:54 -0800

    Add new paragraph

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const products = [
     { id: 1, name: "Mens Pure Black Color Full Sleeves Tshirt For Winter and Summer Soft Stuff", price: "20.99", image: "images/product1.jpg" },
-    { id: 2, name: "Brown Jacket", price: "15.49", image: "images/product2.jpg" },
+    { id: 2, name: "Brown Jacket Men For Winter New Arrival 2025", price: "15.49", image: "images/product2.jpg" },
     { id: 3, name: "White And Black T-Shirt", price: "35.99", image: "images/product3.jpg" },
     { id: 4, name: "Tution Bag", price: "10.99", image: "images/product4.jpg" },
     { id: 5, name: "Plan Basic T-Shirts", price: "25.00", image: "images/product5.jpg" },
@@ -51,4 +51,47 @@ function buyNow(productId) {
 }
 
 
+// Log In Sign Up 
+let users = {};
+function signUp() {
+    let name = document.getElementById('signup-name').value;
+    let email = document.getElementById('signup-email').value;
+    let password = document.getElementById('signup-password').value;
+    
+    if (name && email && password) {
+        users[email] = { name, password };
+        alert('Sign Up Successful!');
+        closeModal('signup-form');
+    } else {
+        alert('Please fill all fields!');
+    }
+}
 
+function logIn() {
+    let name = document.getElementById('login-name').value;
+    let email = document.getElementById('login-email').value;
+    let password = document.getElementById('login-password').value;
+    
+    if (users[email] && users[email].password === password && users[email].name === name) {
+        alert('Welcome to the Shop Xpress website!');
+        closeModal('login-form');
+    } else {
+        alert('Your name, email, or password is incorrect. Please try again!');
+    }
+}
+
+function toggleDropdown() {
+    document.querySelector('.nav-icon').classList.toggle('active');
+}
+
+function openModal(id) {
+    let modal = document.getElementById(id);
+    modal.style.display = 'block';
+    setTimeout(() => modal.classList.add('show'), 50);
+}
+
+function closeModal(id) {
+    let modal = document.getElementById(id);
+    modal.classList.remove('show');
+    setTimeout(() => modal.style.display = 'none', 500);
+}
--- buy-now.js
+++ buy-now.js
@@ -94,3 +94,5 @@ function sendMessage() {
         document.getElementById("userMessage").value = "";
     }
 }
+
+
